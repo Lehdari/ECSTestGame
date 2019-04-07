11cb5554e54e164176ea8793783d47c0dfb8e988
Miika Lehtimäki <miika.lehtimaki@gmail.com>
2019-04-07 17:56:41 +0300

    LogicSystem: Execute Logic

--- src/LogicSystem.cpp
+++ src/LogicSystem.cpp
@@ -4,8 +4,16 @@
 
 #include "LogicSystem.hpp"
 
+#include <cstdio> // TEMP
 
-void LogicSystem::operator()(const EntityId& eId, LogicComponent& ec)
+
+LogicSystem::LogicSystem(Ecs& ecs) :
+    _ecs    (ecs)
 {
+}
 
+void LogicSystem::operator()(const EntityId& eId, LogicComponent& lc)
+{
+    if (lc._logicExecutor)
+        lc._logicExecutor(lc._logic, _ecs, eId);
 }
